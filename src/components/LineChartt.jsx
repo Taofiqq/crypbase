dcad8bfab1539650e3eb0bbd640f5f86c5003df3
User <taofiqaiyelabegan45@gmail.com>
2021-12-14 05:18:37 +0800

    added new styles

--- src/components/LineChartt.jsx
+++ src/components/LineChartt.jsx
@@ -1,6 +1,6 @@
 import { Line } from "react-chartjs-2";
 import { Col, Row, Typography } from "antd";
-import { Chart } from "chart.js/auto";
+// import { Chart } from "chart.js/auto";
 
 const { Title } = Typography;
 
